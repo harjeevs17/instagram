83907792ff1b52a8cd0d37008cd9aa5388cedc43
Singh <harjeevs17@gmail.com>
2023-02-20 22:03:47 +0530

    Updated keys

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGOURI:
-    "mongodb+srv://harjeev:harjeev1@cluster0.hosg8.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://harjeev:harjeev1@cluster0.hosg8.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "whyareyousnoppingbitch",
 };
